369df1a4f855b4541324c467aa8c5fc1a2815901
kivet <wanghuixiang@druid.tech>
2021-05-10 11:39:00 +0800

    feat: 项目初始化

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,18 +2,18 @@
  * @Description: 首页
  * @Author: kivet
  * @Date: 2021-05-08 14:00:33
- * @LastEditTime: 2021-05-10 11:21:55
+ * @LastEditTime: 2021-05-10 11:38:39
  */
-import { FC } from 'react';
-import { connect } from 'react-redux';
-import { View, Text } from '@tarojs/components';
-import { IGlobalState } from '@/models/globals';
-import styles from './index.module.less';
+import { FC } from 'react'
+import { connect } from 'react-redux'
+import { View, Text } from '@tarojs/components'
+import { IGlobalState } from '@/models/globals'
+import styles from './index.module.less'
 
 interface IProps extends IGlobalState {}
 
 const Home: FC<IProps> = (props) => {
-  console.log('global model data:', API_BASE, props);
+  console.log("global model data:", API_BASE, props);
 
   return (
     <View className={styles.container}>
